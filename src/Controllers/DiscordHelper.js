e9542f0695c039582c8ec565f210b2376815d258
Space_Inteprise <44732812+emanuelfranklyn@users.noreply.github.com>
2020-06-05 21:46:12 -0300

    Update DiscordHelper.js

--- src/Controllers/DiscordHelper.js
+++ src/Controllers/DiscordHelper.js
@@ -2,7 +2,7 @@ const Configs = require('../../Configs')
 
 module.exports = {
     Log: (client, Message) => {
-        const channel = client.channels.get(Configs.LogChannel)
+        const channel = client.channels.cache.get(Configs.LogChannel)
         channel.send("`"+Date.now()+"` "+Message)
     }
 }
