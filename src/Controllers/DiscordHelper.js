4a40550a972e1a50bef3e1f1dadbcb49c7dfa497
Space_Inteprise <44732812+emanuelfranklyn@users.noreply.github.com>
2020-07-23 14:45:34 -0300

    Delete DiscordHelper.js

--- src/Controllers/DiscordHelper.js
+++
@@ -1,8 +0,0 @@
-const Configs = require('../../Configs')
-
-module.exports = {
-    Log: (client, Message) => {
-        const channel = client.channels.cache.get(Configs.LogChannel)
-        channel.send("`"+Date.now()+"` "+Message)
-    }
-}
