9665a58c786923e2ed2c071b4524784a3478c6c5
Space_Inteprise <44732812+emanuelfranklyn@users.noreply.github.com>
2020-07-23 14:48:30 -0300

    Add files via upload

--- Main.js
+++ Main.js
@@ -34,18 +34,19 @@ client.on("guildMemberAdd", async User => {
           const baquiground = await canvas.loadImage(Background)
           ctx.drawImage(baquiground, 0,0, Cunvas.width, Cunvas.height)
 
-          let fontSize = 200;
+          let fontSize = 230;
 
           do {
             // Assign the font to the context and decrement it so it can be measured again
             ctx.font = `${fontSize -= 10}px pirates of cydonia`;
             // Compare pixel width of the text to the canvas minus the approximate avatar size
-          } while (ctx.measureText(User.displayName).width > Cunvas.width / (1.6));
+          } while (ctx.measureText(User.displayName).width > Cunvas.width / (1.56));
 
           // Select the style that will be used to fill the text in
+          ctx.textAlign = 'center'
           ctx.fillStyle = '#ffffff';
           // Actually fill the text with a solid color
-          ctx.fillText(User.displayName, Cunvas.width / (2.2+(Number(ctx.measureText(User.displayName).width)/2000)), Cunvas.height / 1.8);
+          ctx.fillText(User.displayName, Cunvas.width / 1.54, Cunvas.height / 1.8);
           if (data.ShowServerName == true) {
             let fontSize2 = 200;
 
@@ -53,13 +54,13 @@ client.on("guildMemberAdd", async User => {
               // Assign the font to the context and decrement it so it can be measured again
               ctx.font = `${fontSize2 -= 10}px pirates of cydonia`;
               // Compare pixel width of the text to the canvas minus the approximate avatar size
-            } while (ctx.measureText(User.guild.name).width > Cunvas.width / (1.20));
+            } while (ctx.measureText(User.guild.name).width > Cunvas.width / (1.54));
 
             // Select the style that will be used to fill the text in
+            ctx.textAlign = 'center'
             ctx.fillStyle = '#CCCCCC';
             // Actually fill the text with a solid color
-            console.log(Number(ctx.measureText(User.guild.name).width/2000))
-            ctx.fillText(User.guild.name, Cunvas.width / (2.2+(Number(ctx.measureText(User.guild.name).width/2000))), Cunvas.height / 1.3);
+            ctx.fillText(User.guild.name, Cunvas.width / 1.54, Cunvas.height / 1.3);
           }
           ctx.beginPath();
           // Start the arc to form a circle
@@ -80,22 +81,29 @@ client.on("guildMemberAdd", async User => {
   }
 })
 client.on('message', async msg => {
-  var message = msg.toString().slice(2, msg.toString().length);
-  var args = message.split(' ')
-  args[0] = args[0].toLowerCase()
-  message = args.join(' ')
-  if (msg.toString().toLowerCase().slice(0,2) === Configs.Prefix) {
-    //Have the prefix the user want to do some thing!
-    var Waiting = msg.channel.send(`${client.emojis.cache.find(emoji => emoji.name === "TalesGardemSpaceIntLoadingGif1")}`)
-    await Hub.ResolveCommand(client, msg, message).then(() => {
-      Promise.resolve(Waiting).then(function(value) {
-        value.delete()
-      }).catch(() => {
-        DisHel.Log(client,"Erro: Não foi possivel deletar: Main.js: value.delete()")
-      })
-    }).catch((e) => {
-      DisHel.Log(client,"Erro: Não foi possivel esperar: Main.js: await Hub.ResolveCommand(client, msg, message)   |  "+e)
-    });
+  if(msg.author.bot) return;
+  let Work = false;
+  let message;
+  let args;
+  Configs.Prefix.forEach(element => {
+    if ((msg.toString().toLowerCase().slice(0,element.length) === element) === true) {
+      Work=true;
+      message = msg.toString().slice(element.length, msg.toString().length);
+      args = message.split(' ')
+      args[0] = args[0].toLowerCase()
+      message = args.join(' ')
+    } else if (element.toString().includes("#")) {
+      if ((element.slice(0,13) === msg.toString().slice(0,13)) || (element.slice(0,5) === msg.toString().slice(0,5))) {
+        Work=true;
+        message = msg.toString().slice(element.length, msg.toString().length);
+        args = message.split(' ')
+        args[0] = args[0].toLowerCase()
+        message = args.join(' ')
+      }
+    }
+  })
+  if (Work === true) {
+    await Hub.ResolveCommand(client, msg, message)
   }
 });
 
